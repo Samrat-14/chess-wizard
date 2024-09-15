2726ffeb5504eea0d788444fe8471d754d679045
Samrat-14 <samratdewdrops0@gmail.com>
2024-09-15 14:45:00 +0530

    updated till chess 1.0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        'primary-color': 'var(--primary-color)',
+        'primary-color-dark': 'var(--primary-color-dark)',
+        'secondary-color': 'var(--secondary-color)',
+        'bg-color': 'var(--bg-color)',
       },
     },
   },
